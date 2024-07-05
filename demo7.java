cc6fe64351d0b5091225eb54f655c4e5e49addb8
modugu ranga sai <rangasai@Siris-Air.lan>
2024-07-05 20:19:23 +0100

    my examples

---
+++ demo7.java
@@ -0,0 +1,18 @@
+package Siri;
+
+
+	public class demo7 {
+	    public static void main(String[] args) {
+	       
+	    	float x = 2.3f;
+	        
+	    	int y = 3;
+	       
+	        float z = x + y;
+	       
+	        System.out.println("z: " + z);
+	    }
+	}
+
+
+
